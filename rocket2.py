8f7731356536502a3cb89104178b81bccb3ac3e6
glenpassow <glenpassow@hanovernorwichschools.org>
2018-03-14 11:04:57 -0400

    Updated from Brython Server: 3/14/2018 11:04:56 AM

--- rocket2.py
+++ rocket2.py
@@ -9,9 +9,17 @@ Re = 6.371E6 #Earth radius in meters
 Me = 5.972E24 #Earth mass in kg
 G = 6.674E-11 #Gravitational constant
 
-#escape velocity
+#Moon stuff
+Rm = 1.737E6
+Mm = 7.35E22
+
+#escape velocity moon
+Vm = sqrt(2*MM*G/Rm)
+print("Predicted moon escape velocity is ", Vm," m/s")
+
+#escape velocity earth
 Ve = sqrt(2*Me*G/Re)
-print("Predicted escape velocity is ", Ve," m/s")
+print("Predicted earth escape velocity is ", Ve," m/s")
 
 #Slider for the timezoom
 tz = Slider((10,400), 0, 5, 0, positioning="physical")
