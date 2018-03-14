3a3a9be11172dfe24a58bdaf43adb0dde29cb0c8
glenpassow <glenpassow@hanovernorwichschools.org>
2018-03-14 08:59:40 -0400

    Updated from Brython Server: 3/14/2018 8:59:38 AM

--- rocket1.py
+++ rocket1.py
@@ -2,5 +2,5 @@
 from ggrocket import Rocket, Planet
 
 earth = Planet (viewscale=0.0005)
-rocket = Rocket(earth, altitude=400000, velocity=1000, timezoom=1)
+rocket = Rocket(earth, altitude=2000000, velocity=6900, timezoom=1)
 earth.run(rocket)
